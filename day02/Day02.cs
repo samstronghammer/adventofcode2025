1c2dc365750d2a4b20f2da191123f1c9f91ab642
samstronghammer <sam@luna-nelson.com>
2025-12-02 10:41:32 -0500

    Day 2

---
+++ day02/Day02.cs
@@ -0,0 +1,57 @@
+class Day02
+{
+    public static void Run()
+    {
+        var input = Util.fileToArray("day02/in.txt");
+        long maxNum = 0;
+        var rangeStrings = input[0].Split(',');
+        var ranges = rangeStrings
+            .ToList()
+            .ConvertAll(
+                (s) =>
+                {
+                    var toks = s.Split('-');
+                    var rangeEnd = long.Parse(toks[1]);
+                    maxNum = long.Max(maxNum, rangeEnd);
+                    return new IdRange(long.Parse(toks[0]), rangeEnd);
+                }
+            );
+        long currNum = 1;
+        var invalidIds1 = new HashSet<long>();
+        var invalidIds2 = new HashSet<long>();
+        while (true)
+        {
+            var repetitions = 2;
+            while (true)
+            {
+                var invalidIdGuess = long.Parse(
+                    string.Concat(Enumerable.Repeat(currNum.ToString(), repetitions))
+                );
+                if (invalidIdGuess > maxNum)
+                    break;
+                if (
+                    ranges.Any(
+                        (range) => invalidIdGuess >= range.Start && invalidIdGuess <= range.End
+                    )
+                )
+                {
+                    if (repetitions == 2)
+                        invalidIds1.Add(invalidIdGuess);
+                    invalidIds2.Add(invalidIdGuess);
+                }
+                repetitions++;
+            }
+            if (repetitions == 2)
+                break;
+            currNum++;
+        }
+        Console.WriteLine($"Part 1: {invalidIds1.Sum()}");
+        Console.WriteLine($"Part 2: {invalidIds2.Sum()}");
+    }
+}
+
+readonly struct IdRange(long start, long end)
+{
+    public long Start { get; } = start;
+    public long End { get; } = end;
+}
